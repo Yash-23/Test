eb878fd3179b14ada62990903a97688899c1770c
Yash Sakle <yash.sakle23@gmail.com>
2023-01-05 17:16:53 +0530

    Final Commits

--- DesignPattern/FactoryPatternDemo.java
+++ DesignPattern/FactoryPatternDemo.java
@@ -2,11 +2,11 @@ public class FactoryPatternDemo {
     public static void main(String args[]){
         
         AccountFactory bankAcc = new AccountFactory();
-
+        
         bankAccount acc1 = AccountFactory.createAccount("Personal");
 
         acc1.create();
-        
+
         bankAccount acc2 = AccountFactory.createAccount("Cooperate");
         
         acc2.create();
